1a3ad053f69703d1157776d6a8d0d51c0ee29ae3
Cristopher Ettienne Scott Mendoza <crisscottmendoza@gmail.com>
2025-07-20 14:23:09 -0600

    Setup GitHub Action for S3 deployment

--- src/components/theme/Footer.tsx
+++ src/components/theme/Footer.tsx
@@ -5,7 +5,7 @@ export const Footer = () => {
         <footer className="w-full border-t border-zinc-200 dark:border-zinc-800 bg-white dark:bg-zinc-900 py-6 text-center text-sm text-zinc-500 dark:text-zinc-400">
             <p>
                 © {new Date().getFullYear()} Cristopher Scott. Todos los
-                derechos reservados.
+                derechos reservados. v1.0.0
             </p>
         </footer>
     );
